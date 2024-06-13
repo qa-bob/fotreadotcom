c11fc7bea5d32adfc639f00e2bfde65d955973da
Bob Small <rlsmall@cox.net>
2024-06-13 08:52:46 -0700

    added new assert to HomePage

--- src/test/java/SciconSolutionsHomePage.java
+++ src/test/java/SciconSolutionsHomePage.java
@@ -9,7 +9,7 @@ public class SciconSolutionsHomePage {
     @Test
     public void testFortrea() {
 
-        WebDriver driver = new ChromeDriver(); //TODO: Add this as part of the @BeforeTest routine
+        WebDriver driver = getWebDriver();
         driver.get("https://www.fortrea.com/");  //TODO: Add this as part of the @BeforeTest routine
         //TODO: Need to handle the Cookies Prompt, and we should create a class to do this
         driver.manage().window().maximize();
@@ -17,5 +17,23 @@ public class SciconSolutionsHomePage {
         Assert.assertTrue(pageTitle.isEnabled());
         //TODO: Create the logging of the test:Name, App, feature, environment, result, type
         driver.quit();  //TODO: Add this as part of the @AfterTest routine
+    }
+
+    private static WebDriver getWebDriver() {
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+
+    @Test
+    public void testFortreaPageText(){
+        WebDriver driver = getWebDriver();
+        driver.get("https://www.fortrea.com/");
+        driver.manage().window().maximize();
+        WebElement pageText = driver.findElement(By.cssSelector("div > h1"));
+        String pageTextExpected = "Solutions that bring life-changing treatments to patients faster";
+        String pageTextAcual = pageText.getText();
+        Assert.assertEquals(pageTextAcual, pageTextExpected, "Page Text is not the same as expected");
+        //Assert.assertEquals(pageTextAcual.contains(pageTextExpected), "Page Text is not the same as expected");
+        driver.quit();
     }
 }
