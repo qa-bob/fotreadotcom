99dff077ecd92c3482ead938e9b10dda7c45dde3
Bob Small <rlsmall@cox.net>
2024-06-13 10:57:43 -0700

    updated BaseClass and HomePage

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,15 +1,29 @@
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 public class BaseClass {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    static WebDriver getWebDriver() {
+        driver = new ChromeDriver();
+        driver.get("https://www.fortrea.com/");
+        driver.manage().window().maximize();
+        return driver;
+    }
+
     @BeforeMethod
     public void beforeMethod() {
         System.out.println("BaseClass's Before Test method");
 
     }
 
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
     @AfterMethod
     public void afterMethod() {
         System.out.println("BaseClass's After Test method");
--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -5,29 +5,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class HomePage {
+public class HomePage extends BaseClass{
     @Test
     public void testFortrea() {
 
         WebDriver driver = getWebDriver();
-        driver.get("https://www.fortrea.com/");  //TODO: Add this as part of the @BeforeTest routine
-        //TODO: Need to handle the Cookies Prompt, and we should create a class to do this
-        driver.manage().window().maximize();
         WebElement pageTitle = driver.findElement(By.cssSelector("head > title"));
         Assert.assertTrue(pageTitle.isEnabled());
         //TODO: Create the logging of the test:Name, App, feature, environment, result, type
         driver.quit();  //TODO: Add this as part of the @AfterTest routine
     }
 
-    private static WebDriver getWebDriver() {
-        return new ChromeDriver();
-    }
-
     @Test
     public void testFortreaPageText(){
         WebDriver driver = getWebDriver();  //TODO: Need to not build a new driver for each Test
-            driver.get("https://www.fortrea.com/");
-            driver.manage().window().maximize();
         WebElement pageText = driver.findElement(By.cssSelector("div > h1"));
         String pageTextExpected = "Solutions that bring life-changing treatments to patients faster";
         String pageTextAcual = pageText.getText();
